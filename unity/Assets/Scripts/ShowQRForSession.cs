23265e9314d3523f6a9a96038fb24a5d6a5897ec
lucas-croquet <123010049+lucas-croquet@users.noreply.github.com>
2023-05-31 16:13:46 -0700

    add local reflector logic

--- unity/Assets/Scripts/ShowQRForSession.cs
+++ unity/Assets/Scripts/ShowQRForSession.cs
@@ -10,17 +10,21 @@ public class ShowQRForSession : MonoBehaviour
         if (qrShower != null)
         {
             CroquetRunner runner = CroquetBridge.Instance.GetComponent<CroquetRunner>();
-            string localReflector = runner.localReflector;
+            string localReflector = PlayerPrefs.GetString("sessionIP", "");
             int sessionNameValue = PlayerPrefs.GetInt("sessionNameValue", 1);
             string url;
             if (localReflector == "")
             {
+                Debug.Log("local reflector session ip setting empty, using live croquet network");
                 url = $"https://croquet.io/demolition-multi/?q={sessionNameValue}";
             }
             else
             {
+                Debug.Log("local reflector session ip setting found, using set ip");
                 url = $"http://{localReflector}/demolition-multi?q={sessionNameValue}&reflector=ws://{localReflector}/reflector&files=http://{localReflector}/files";
             }
+            
+            Debug.Log($"Displaying QR: Session Name Value Loaded: {sessionNameValue} with reflector: {url}");
             qrShower.DisplayQR(url);
         }
     }
