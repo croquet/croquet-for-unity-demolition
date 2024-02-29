b2e4a66debe5088a1f5cd1ce49e07d848cb88232
Aran Lunzer <aran@acm.org>
2024-02-28 18:30:28 -0800

    redundant getter in actor

--- unity/Assets/CroquetJS/demolition/Models.js
+++ unity/Assets/CroquetJS/demolition/Models.js
@@ -20,7 +20,6 @@ export class GameActor extends mix(Actor).with(AM_Spatial, AM_RapierRigidBody) {
     get type() { return this._type || "primitiveCube" }
     get color() { return this._color || [0.5, 0.5, 0.5] }
     get alpha() { return this._alpha === undefined ? 1 : this._alpha }
-    get rigidBodyType() { return this._rigidBodyType }
 }
 GameActor.register('GameActor');
 
